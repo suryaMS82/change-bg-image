42faa2ee24486510837555ceef731cf62d96120a
surya narayana reddy <suryanr1999@gmail.com>
2021-06-05 18:21:24 +0530

    random bg image

---
+++ script.js
@@ -0,0 +1,9 @@
+var button = document.querySelector('.btn');
+var body= document.querySelector('body');
+button.addEventListener('click',bgchange);
+var img = ["url('/images/image-1.jpg')","url('/images/image-2.jpg')","url('/images/image-3.jpg')","url('/images/image-5.jpg')","url('/images/image-4.jpg')","url('/images/image-6.jpg')","url('/images/image-7.jpg')","url('/images/image-8.jpg')","url('/images/image-9.jpg')"];
+
+function bgchange(){
+    var random= Math.floor(Math.random()*img.length);
+    body.style.backgroundImage = img[random];
+}
