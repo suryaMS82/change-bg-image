170d26ec2e4fe9d61eda55b93949e6b1ba2ab60e
surya <55597568+suryaMS82@users.noreply.github.com>
2021-06-09 12:50:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var button = document.querySelector('.btn');
 var body= document.querySelector('body');
 button.addEventListener('click',bgchange);
-var img = ["url('/images/image-1.jpg')","url('/images/image-2.jpg')","url('/images/image-3.jpg')","url('/images/image-5.jpg')","url('/images/image-4.jpg')","url('/images/image-6.jpg')","url('/images/image-7.jpg')","url('/images/image-8.jpg')","url('/images/image-9.jpg')"];
+var img = ["url('images/image-1.jpg')","url('images/image-2.jpg')","url('images/image-3.jpg')","url('images/image-5.jpg')","url('images/image-4.jpg')","url('images/image-6.jpg')","url('images/image-7.jpg')","url('images/image-8.jpg')","url('images/image-9.jpg')"];
 
 function bgchange(){
     var random= Math.floor(Math.random()*img.length);
